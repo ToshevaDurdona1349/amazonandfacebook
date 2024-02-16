1c5cb6e6554510bcd81a2f63f1d1a97393d633a0
ToshevaDurdona <toshevadurdona2002@gmail.com>
2024-02-16 12:47:52 +0500

    amazon and facebook

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:untitled6/pages/amazon_page.dart';
+import 'package:untitled6/pages/facebook_page.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home:FacebookPage  (),
+
+    );
+  }
+}
